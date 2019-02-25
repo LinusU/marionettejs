91b7157fbb9d89c9402932031601c125152bd8f3
Linus Unnebäck <linus@folkdatorn.se>
2019-02-25 12:56:40 +0100

    🎉 Bump Swift NAPI bindings to 1.0.0-alpha.4

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/LinusU/Marionette", from: "1.0.0-alpha.10"),
-        .package(url: "https://github.com/LinusU/swift-napi-bindings", from: "1.0.0-alpha.3"),
+        .package(url: "https://github.com/LinusU/swift-napi-bindings", from: "1.0.0-alpha.4"),
     ],
     targets: [
         .target(name: "MarionetteTrampoline", dependencies: ["NAPIC", "NAPI"], path: "Sources/MarionetteTrampoline"),
